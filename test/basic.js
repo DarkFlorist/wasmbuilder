a2bda70a874fa3a9fa8750bd52cd4a875e8ff1fe
John Doe <john@doe.org>
2025-02-01 13:48:02 +0000

    test: update to new deps

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,6 @@
-const assert = require("assert");
-
-const ModuleBuilder = require("../index.js");
+import { describe, it } from 'micro-should';
+import { deepStrictEqual } from 'node:assert';
+import ModuleBuilder from '../index.js';
 
 function buf2hex(buffer) { // buffer is an ArrayBuffer
     return Array.prototype.map.call(new Uint8Array(buffer), x => ("00" + x.toString(16)).slice(-2)).join("");
@@ -12,6 +12,7 @@ describe("Basic tests for WasmGenerator", () => {
 
         const bytes = module.build();
 
-        assert.equal("0061736d01000000010100020f0103656e76066d656d6f72790200010301000701000a01000b0a010041000b0408000000",buf2hex(bytes) );
+        deepStrictEqual("0061736d01000000010100020f0103656e76066d656d6f72790200010301000701000a01000b0a010041000b0408000000",buf2hex(bytes) );
     });
 });
+it.runWhen(import.meta.url);
